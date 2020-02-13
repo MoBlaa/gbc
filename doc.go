8d4ca0f6343c4c8502478c9b7c78326dd5b108c0
mo_blaa <moblaa@gmx.net>
2020-02-13 22:31:06 +0100

    extended package documentation.

--- doc.go
+++ doc.go
@@ -1,2 +1,8 @@
 // Package gbc is a connector library for chat providers mainly supporting development of bots.
+//
+// It's aimed to support the basic operations like sending and receiving messages, as well
+// as adjusting message output to the platform to their messaging rates.
+//
+// Currently only Twitch IRC Chat is supported with messaging rates and automatically replying
+// to `PING` messages.
 package gbc
